8e0df1a1e66920a24cb05e7da67f193588ff545d
Junyoung Kwon <jkwon031@ucr.edu>
2019-01-10 11:40:52 -0800

    Initial commit

---
+++ jkwon031_jmend062_lab2_part1/jkwon031_jmend062_lab2_part1/main.c
@@ -0,0 +1,18 @@
+/*
+ * jkwon031_jmend062_lab2_part1.c
+ *
+ * Created: 1/10/2019 11:38:48 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
