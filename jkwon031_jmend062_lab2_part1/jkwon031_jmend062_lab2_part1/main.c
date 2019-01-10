6bc18751a5a80f5e0d176cf4fd102acc0abb3d9a
Junyoung Kwon <jkwon031@ucr.edu>
2019-01-10 12:36:11 -0800

    Made Exercise 1

--- jkwon031_jmend062_lab2_part1/jkwon031_jmend062_lab2_part1/main.c
+++ jkwon031_jmend062_lab2_part1/jkwon031_jmend062_lab2_part1/main.c
@@ -11,8 +11,24 @@
 int main(void)
 {
     /* Replace with your application code */
-    while (1) 
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+    
+	unsigned char tempA1 = 0x00;
+	unsigned char tempA2 = 0x00;
+	unsigned char tempB = 0x00;
+	while (1) 
     {
-    }
+		tempA1 = PINA & 0x01; // PA0
+		tempA2 = PINA & 0x02; // PA1
+		
+		if (tempA2 == 0x00 && tempA1 == 0x01){
+			tempB = (tempB & 0xFC) | 0x01;
+		}else{
+			tempB = (tempB & 0xFC) | 0x00;
+		}
+		PORTB = tempB;
+	}
+	return 0;
 }
 
