280b323b47410a75bb7e35f16ebf6e3c8b049707
Junyoung Kwon <jkwon031@ucr.edu>
2019-01-22 15:34:57 -0800

    Practice

---
+++ Others/lab_chip/lab_chip/main.c
@@ -0,0 +1,19 @@
+/*
+ * lab_chip.c
+ *
+ * Created: 1/22/2019 12:09:01 PM
+ * Author : ericj
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+	while(1)
+	{
+		PORTB = 0x0F; // Writes port B's 8 pins with 00001111
+	}
+}
+
+
